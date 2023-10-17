c7c6464f8ec931412a27561593dadc8cf93acee2
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-10-17 13:48:15 -0500

    working on ecommerce page

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full '>
             <Navbar/>
             </div>
-          </div>
 
           <div>
             <Routes>
@@ -77,7 +76,7 @@ const App = () => {
 
             </Routes>
           </div>
-
+          </div>
         </div>
       </BrowserRouter>
     </div>
