e703f6b6bdd63b0a4d6fc46a50b7e1d336299095
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-10-22 09:51:49 -0500

    working on ecommerce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -31,6 +31,16 @@ const Ecommerce = () => {
             <div
               key={item.title} className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'
             >
+              <button type='button' style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'>
+                {item.icon}
+              </button>
+              <p className='mt-3'>
+                <span className='text-lg font-semibold'>
+                  {item.amount}
+                </span>
+
+              </p>
             </div>
           ))}
 
