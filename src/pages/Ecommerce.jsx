1a106b1b043ee4e1d335e9b0d784641c9cd92eb4
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-10-23 13:33:17 -0500

    working on homepage

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -7,7 +7,7 @@ import { useStateContext } from '../contexts/ContextProvider'
 
 const Ecommerce = () => {
   return (
-    <div className='mt-12'>
+    <div className='mt-24'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
         <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
           <div className='flex justify-between items-center'>
@@ -15,8 +15,13 @@ const Ecommerce = () => {
               <p className='font-bold text-gray-400'>Earnings</p>
               <p className='text-2xl'>$63,448.78</p>
             </div>
+            <button
+              type='button'
+              className='text-2xl opacity-0.9 text-white hover: drop-shadow-xl rounded-full p-4'>
+                <BsCurrencyDollar />
+              </button>
           </div>
-          <div>
+          <div className='mt-6'>
             <Button
             color="white"
             bgColor="blue"
@@ -39,8 +44,11 @@ const Ecommerce = () => {
                 <span className='text-lg font-semibold'>
                   {item.amount}
                 </span>
-
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
               </p>
+              <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
             </div>
           ))}
 
