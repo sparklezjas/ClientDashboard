f5b93f44809fbfc731dc88b1b50a0a9be721542a
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-10-29 08:30:45 -0500

    working on ecommerce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -72,10 +72,16 @@ const Ecommerce = () => {
               <div className='border-r-1 border-color m-4 pr-10'>
                 <div>
                   <p>
-                    <span className='text-font-semibold'>$93,438</span>
-                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full tect-white bg-green-400'>23%</span>
+                    <span className='text-3xl-font-semibold'>$93,438</span>
+                    <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
                   </p>
-                  <p>Budget</p>
+                  <p className='text-gray-500 mt-1'>Budget</p>
+                </div>
+                <div className='mt-8'>
+                  <p>
+                    <span className='text-3xl-font-semibold'>$48,438</span>
+                  </p>
+                  <p className='text-gray-500 mt-1'>Expense</p>
                 </div>
               </div>
             </div>
