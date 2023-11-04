bf0f9f850152fed5d4d4135f8f6d28fca1c9ffe1
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-11-04 17:29:30 -0500

    Modifying order and header

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+import { GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
   )
 }
 
