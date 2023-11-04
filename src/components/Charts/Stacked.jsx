cc4a1e3663bd7393e90238864f8bdf9f7405102e
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-11-03 21:41:57 -0500

    Finish stacked component, starting on orders

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -7,7 +7,7 @@ const Stacked = ({ width, height }) => {
     <ChartComponent
       width={width}
       height={height}
-      id='stack chart'
+      id='charts'
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
       chartArea={{ border: {width:0 }}}
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy'
 
 const Orders = () => {
   return (
