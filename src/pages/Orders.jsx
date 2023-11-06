302c15099a3768cdaf5f4ba23815837e4a3c6bf3
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-11-06 16:30:49 -0600

    adding functionality to orders component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -8,12 +8,16 @@ const Orders = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item}/>
           ))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, PdfExport, Edit, ExcelExport]} />
       </GridComponent>
     </div>
   )
