1ed04a42005869ccbb8c892f5f2fbfba6db958c5
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-11-10 17:49:32 -0600

    Imports

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { ScheduleComponent, ViewDirective, ViewsDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule'
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars'
+import { scheduleData } from '../data/dummy'
 
 const Calendar = () => {
   return (
