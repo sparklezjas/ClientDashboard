6ac8c79413d5edc4bfcc1ced18ad287f0b5f8636
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-11-15 09:30:57 -0600

    imports for kanban

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban'
+import { kanbanData, kanbanGrid } from '../data/dummy'
+import { Header } from '../components'
 const Kanban = () => {
   return (
     <div>Kanban</div>
