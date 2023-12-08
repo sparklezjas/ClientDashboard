fe96357a2b0c03d2d5cc08eba2ac4c49d81ef2e6
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-12-08 12:09:15 -0600

    working on theme

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -38,6 +38,20 @@ const ThemeSettings = () => {
         </div>
         <div className='flex-col border-t-1 border-color p-4 ml-4'>
           <p className='font-semibold text-lg'>Theme Colors</p>
+            <div className='flex gap-3'>
+              {themeColors.map((item, index) => (
+                <TooltipComponent key={index}
+                content={item.name}
+                position='TopCenter'
+                >
+                  <div className='relative mt-2 cursor-pointer flex gap-5 items-center'>
+                    <button>
+                      
+                    </button>
+                  </div>
+                </TooltipComponent>
+              ))}
+            </div>
         </div>
       </div>
     </div>
