11d2761bcdd2cbd3d3a4eedec8b2398a69d24dce
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-12-12 07:18:15 -0600

    working on theme and context

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,19 @@ export const ContextProvider = ({children}) => {
     const[activeMenu, setActiveMenu] = useState (true)
     const [isClicked, setIsClicked] = useState(initialState)
     const [screenSize, setScreenSize] = useState (undefined)
+    const [currentColor, setCurrentColor] = useState('#03C9D7')
+    const [currentMode, setCurrentMode] = useState('Light')
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value)
+        localStorage.setItem('themeMode', e.target.value)
+    }
+
+    const setColor = (e) => {
+        setCurrentColor(e.target.value)
+        localStorage.setItem('colorMode', e.target.value)
+    }
+
 
     const handleClick = (clicked) => {
         setIsClicked({...initialState, [clicked]: true})
@@ -26,8 +39,9 @@ export const ContextProvider = ({children}) => {
                 isClicked,
                 setIsClicked,
                 handleClick,
-                screenSize,
-                setScreenSize
+                screenSize, setScreenSize,
+                currentColor, currentMode,
+                setCurrentColor, setCurrentMode
             }}
         >
             {children}
