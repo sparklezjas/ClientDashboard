377a86730742896b66f95774e8eaeb84f354a891
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-12-23 17:42:04 -0600

    finish theme and dark mode

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     <div className={currentMode === 'Dark' ? 'dark' : ''}> 
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
-          <div className='fixed right-f bottom-4' style={{zIndex: '1000'}}>
+          <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
             <TooltipComponent content="Settings" position="Top">
               <button type='button'
               className='text-3xl p-3
@@ -41,8 +41,10 @@ const App = () => {
             </div>
           )}
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ?
-              'md:ml-72' : 'flex-2'}`
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+            ${activeMenu 
+              ? 'md:ml-72' 
+              : 'flex-2'}`
           }>
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full '>
             <Navbar/>
