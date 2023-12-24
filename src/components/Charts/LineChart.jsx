4d6f91c095e8d3a1f1cc899e0eab9eaaec6ec538
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-12-24 11:37:47 -0600

    working on line chart

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts'
+import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy'
 
 const LineChart = () => {
   return (
-    <div>LineChart</div>
+      <ChartComponent>
+      </ChartComponent>
   )
 }
 
