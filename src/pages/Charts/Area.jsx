8818eaa4d2bc0ba9a7bd59517dc2ad930b76fbec
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-12-31 12:18:15 -0600

    finished line chart, started on area

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, SplineAreaSeries } from '@syncfusion/ej2-react-charts'
+import { areaCustomSeries, AreaPrimaryYAxis, AreaPrimaryXAxis } from '../../data/dummy'
+import { Header } from '../../components'
+import { useStateContext } from '../../contexts/ContextProvider'
 
 const Area = () => {
+  const { currentMode } = useStateContext()
+
   return (
-    <div>Area</div>
+      <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={AreaPrimaryXAxis}
+      primaryYAxis={AreaPrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      >
+        <Inject services={[LineSeries, DateTime, Legend, Tooltip]}/>
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) =>
+          <SeriesDirective key={index} {...item} /> )}
+        </SeriesCollectionDirective>
+      </ChartComponent>
   )
 }
 
